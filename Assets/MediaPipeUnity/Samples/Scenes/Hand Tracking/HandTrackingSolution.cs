a6145264b8daed0699c89c5f6f25301c78ff969d
XIGUA <xujun55@outlook.com>
2022-05-15 00:23:02 +0800

    add rotation feature for hand tracking

--- Assets/MediaPipeUnity/Samples/Scenes/Hand Tracking/HandTrackingSolution.cs
+++ Assets/MediaPipeUnity/Samples/Scenes/Hand Tracking/HandTrackingSolution.cs
@@ -14,6 +14,8 @@ namespace Mediapipe.Unity.HandTracking
     {
         public GameObject objectEndEffector;
         private EndEffector endEffector;        // 末端执行器类对象
+        private float lastDistance = -1;
+        private float threshold = 0.01f;
 
 
         [SerializeField] private DetectionListAnnotationController _palmDetectionsAnnotationController;
@@ -74,6 +76,31 @@ namespace Mediapipe.Unity.HandTracking
             graphRunner.AddTextureFrameToInputStream(textureFrame);
         }
 
+        private float squareEuclidean(NormalizedLandmark p1, NormalizedLandmark p2)
+        {
+            return Mathf.Pow(p1.X - p2.X, 2) + Mathf.Pow(p1.Y - p2.Y, 2) + Mathf.Pow(p1.Z - p2.Z, 2);
+        }
+
+        private int fingerNumRecognizer(float thresDistance, float indexDistance, float middleDistance, float ringDistance, float pinkyDistance)
+        {
+            if (thresDistance < indexDistance && thresDistance > middleDistance && thresDistance > ringDistance && thresDistance > pinkyDistance)
+            {
+                return 1;
+            }
+            else if (thresDistance < indexDistance && thresDistance < middleDistance && thresDistance > ringDistance && thresDistance > pinkyDistance)
+            {
+                return 2;
+            }
+            else if (thresDistance > indexDistance && thresDistance < middleDistance && thresDistance < ringDistance && thresDistance < pinkyDistance)
+            {
+                return 3;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
         protected override IEnumerator WaitForNextValue()
         {
             List<Detection> palmDetections = null;
@@ -92,45 +119,98 @@ namespace Mediapipe.Unity.HandTracking
                 yield return new WaitUntil(() => graphRunner.TryGetNext(out palmDetections, out handRectsFromPalmDetections, out handLandmarks, out handWorldLandmarks, out handRectsFromLandmarks, out handedness, false));
             }
 
-            _palmDetectionsAnnotationController.DrawNow(palmDetections);
-            _handRectsFromPalmDetectionsAnnotationController.DrawNow(handRectsFromPalmDetections);
+            // _palmDetectionsAnnotationController.DrawNow(palmDetections);
+            // _handRectsFromPalmDetectionsAnnotationController.DrawNow(handRectsFromPalmDetections);
             _handLandmarksAnnotationController.DrawNow(handLandmarks, handedness);
             // TODO: render HandWorldLandmarks annotations
-            _handRectsFromLandmarksAnnotationController.DrawNow(handRectsFromLandmarks);
+            // _handRectsFromLandmarksAnnotationController.DrawNow(handRectsFromLandmarks);
             if (handLandmarks != null)
             {
-                float indexFingerX = handLandmarks[0].Landmark[8].X;
-                float indexFingerY = handLandmarks[0].Landmark[8].Y;
-                float indexFingerZ = handLandmarks[0].Landmark[8].Z;
-                if (lastZ != 0)
+                if (handLandmarks.Count == 1)
                 {
-                    float deltaX = (indexFingerX - lastX) * 100;
-                    float deltaY = (indexFingerY - lastY) * 100;
-                    float deltaZ = (indexFingerZ - lastZ) * 200;
-                    if (deltaX < 1 && deltaX > -1)
+                    lastDistance = -1;
+
+                    float indexFingerX = handLandmarks[0].Landmark[8].X;
+                    float indexFingerY = handLandmarks[0].Landmark[8].Y;
+                    float indexFingerZ = handLandmarks[0].Landmark[8].Z;
+                    if (lastZ != 0)
                     {
-                        deltaX = 0;
+                        float deltaX = (indexFingerX - lastX) * 100;
+                        float deltaY = (indexFingerY - lastY) * 100;
+                        float deltaZ = (indexFingerZ - lastZ) * 200;
+                        if (deltaX < 1 && deltaX > -1)
+                        {
+                            deltaX = 0;
+                        }
+                        if (deltaY < 1 && deltaY > -1)
+                        {
+                            deltaY = 0;
+                        }
+                        if (deltaZ < 1 && deltaZ > -1)
+                        {
+                            deltaZ = 0;
+                        }
+                        endEffector.moveInCameraTrans(new Vector3(deltaX, -deltaY, -deltaZ), new Vector3(0, 0, 0));
                     }
-                    if (deltaY < 1 && deltaY > -1)
-                    {
-                        deltaY = 0;
-                    }
-                    if (deltaZ < 1 && deltaZ > -1)
-                    {
-                        deltaZ = 0;
-                    }
-                    Debug.Log(deltaZ);
-                    endEffector.moveInCameraTrans(new Vector3(deltaX, -deltaY, -deltaZ), new Vector3(0, 0, 0));
+                    lastX = indexFingerX;
+                    lastY = indexFingerY;
+                    lastZ = indexFingerZ;
                 }
-                lastX = indexFingerX;
-                lastY = indexFingerY;
-                lastZ = indexFingerZ;
-            }
-            else
-            {
-                lastX = 0;
-                lastY = 0;
-                lastZ = 0;
+                else if (handLandmarks.Count == 2)
+                {
+                    // 阈值距离(中指第三关节到手腕的距离)
+                    float thresDistance = squareEuclidean(handLandmarks[0].Landmark[0], handLandmarks[0].Landmark[10]);
+                    // 食指到手腕的距离
+                    float indexDistance = squareEuclidean(handLandmarks[0].Landmark[0], handLandmarks[0].Landmark[8]);
+                    // 中指到手腕的距离
+                    float middleDistance = squareEuclidean(handLandmarks[0].Landmark[0], handLandmarks[0].Landmark[12]);
+                    // 无名指到手腕的距离
+                    float ringDistance = squareEuclidean(handLandmarks[0].Landmark[0], handLandmarks[0].Landmark[16]);
+                    // 小拇指到手腕的距离
+                    float pinkyDistance = squareEuclidean(handLandmarks[0].Landmark[0], handLandmarks[0].Landmark[20]);
+
+                    int number = fingerNumRecognizer(thresDistance, indexDistance, middleDistance, ringDistance, pinkyDistance);
+
+                    float distance = Mathf.Sqrt(squareEuclidean(handLandmarks[1].Landmark[4], handLandmarks[1].Landmark[8]));
+                    float degree = 0;
+                    
+                    if(lastDistance == -1)
+                    {
+                        lastDistance = distance;
+                    }
+                    else if (distance - lastDistance > threshold)
+                    {
+                        degree = (distance - lastDistance) * 180;
+                    }
+                    else if (distance - lastDistance < -threshold)
+                    {
+                        degree = (distance - lastDistance) * 180;
+                    }
+
+                    if (degree != 0)
+                    {
+                        if (number == 1)
+                        {
+                            endEffector.moveInLocalTrans(new Vector3(0, 0, 0), new Vector3(degree, 0, 0));
+                        }
+                        else if (number == 2)
+                        {
+                            endEffector.moveInLocalTrans(new Vector3(0, 0, 0), new Vector3(0, degree, 0));
+                        }
+                        else if (number == 3)
+                        {
+                            endEffector.moveInLocalTrans(new Vector3(0, 0, 0), new Vector3(0, 0, degree));
+                        }
+                        lastDistance = distance;
+                    }
+                }
+                else
+                {
+                    lastDistance = -1;
+                    lastX = 0;
+                    lastY = 0;
+                    lastZ = 0;
+                }
             }
         }
 
